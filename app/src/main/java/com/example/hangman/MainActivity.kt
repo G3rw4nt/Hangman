93ed1675d25ce4946529d13c910622198d75caf1
G3rw4nt <krzysztof.dereszynski@gmail.com>
2022-04-14 00:52:56 +0200

    Initial commit

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.hangman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var stringArray: Array<String>
+    val MAX_MISTAKES = 10
+    val wordToFind = ""
+    val usedLetters:CharArray = charArrayOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        stringArray = resources.getStringArray(R.array.words)
+        val hangmanView:ImageView = findViewById(R.id.hangmanView)
+    }
+    fun getRandomWord():String{
+        val number = (0..stringArray.size).random()
+        return stringArray[number]
     }
 }
